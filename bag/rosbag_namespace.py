fae67a6a2e37cb8a4a0aa16eede185a1a43f05a7
Taeyoung96 <tyoung96@yonsei.ac.kr>
2024-01-16 14:16:34 +0900

    add commnets

--- bag/rosbag_namespace.py
+++ bag/rosbag_namespace.py
@@ -48,9 +48,9 @@ def add_namespace_to_topics(input_bag_path, output_bag_path, namespace):
 
 if __name__ == "__main__":
     rospy.init_node('bag_namespace_adder', anonymous=True)
-    input_bag_path = '/root/catkin_ws/src/bag/robot2_2024-01-13-09-49-21.bag'  # 원본 bag 파일 경로
-    output_bag_path = '/root/catkin_ws/src/bag/robot2.bag'  # 결과 bag 파일 경로
-    namespace = 'robot2'  # 추가할 네임스페이스
+    input_bag_path = '/root/catkin_ws/src/bag/robot2_2024-01-13-09-49-21.bag'  # Original bag file path (absolute path inside the docker container)
+    output_bag_path = '/root/catkin_ws/src/bag/robot2.bag'  # Result bag file path (absolute path inside the docker container)
+    namespace = 'robot2'  # Add namespace
 
     add_namespace_to_topics(input_bag_path, output_bag_path, namespace)
     print("Bag file with added namespace is saved as {}".format(output_bag_path))
